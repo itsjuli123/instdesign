fc389721f30740402c728d1ff3cc5d23dcb5c8c0
itsjuli123 <itsjuli@mac.com>
2025-05-20 20:59:20 +0000

    trying to fix broken carousel

--- js/global.js
+++ js/global.js
@@ -194,6 +194,26 @@
             });
         }
     });
+$('.js-slick-story').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
 
 
     // Config Animsition
